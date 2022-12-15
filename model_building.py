0f4782f7bcc6d8f918b0b7605dfe9de1c7a688f6
Norb Lara <norblara@gmail.com>
2022-12-15 13:02:01 -0600

    completed model for glassdoor prediction

--- model_building.py
+++ model_building.py
@@ -2,16 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import statsmodels.api as sm
-from sklearn.model_selection import train_test_split, cross_val_score
+import pickle
+from sklearn.model_selection import GridSearchCV, train_test_split, cross_val_score
 from sklearn.linear_model import LinearRegression, Lasso
-
-
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
 # https://www.statsmodels.org/dev/generated/statsmodels.regression.linear_model.OLS.html
 # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
 # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_validate.html
 # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Lasso.html
+# https://scikit-learn.org/0.16/modules/generated/sklearn.ensemble.RandomForestRegressor.html
+# https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
 
 df = pd.read_csv('eda_data.csv')
 
@@ -38,20 +41,58 @@ lm.fit(X_train, y_train)
 np.mean(cross_val_score(lm, X_train, y_train, scoring='neg_mean_absolute_error', cv=3))
 
 # lasso regression
-lm_l = Lasso()
+lm_l = Lasso(alpha =.13)
 lm_l.fit(X_train,y_train)
 np.mean(cross_val_score(lm_l, X_train, y_train, scoring='neg_mean_absolute_error', cv=3))
 alpha = []
 error = []
+
 for i in range(1,100):
     alpha.append(i/100)
     lml = Lasso(alpha=(i/100))
-    error.append(np.mean(cross_val_score(lml,X_train,y_train, scoring = 'neg_mean_absolute_error', cv= 3)))
-    
+    error.append(np.mean(cross_val_score(lml,X_train,y_train, scoring = 'neg_mean_absolute_error', cv= 3)))    
 plt.plot(alpha,error)
+
 err = tuple(zip(alpha,error))
 df_err = pd.DataFrame(err, columns = ['alpha', 'error'])
 df_err[df_err.error == max(df_err.error)]
+
 # random forest
-# tune models GridsearchCV
+rf = RandomForestRegressor()
+np.mean(cross_val_score(rf, X_train, y_train, scoring='neg_mean_absolute_error', cv=3))
+# this score is 4-5 points less than previous model
+
+# tune random forest model with GridsearchCV 
+# [mse, mae (criterion)], & max_features deprecated
+parameters = {'n_estimators': range(10,300,10), 'criterion':('squared_error','absolute_error')}
+gs = GridSearchCV(rf,parameters, scoring='neg_mean_absolute_error', cv=3)
+gs.fit(X_train,y_train)
+
+gs.best_score_
+gs.best_estimator_
+gs.best_params_
+
 # test ensembles
+tpred_lm = lm.predict(X_test)
+tpred_lml = lm_l.predict(X_test)
+tpred_rf = gs.best_estimator_.predict(X_test)
+
+mean_absolute_error(y_test,tpred_lm)
+mean_absolute_error(y_test,tpred_lml)
+mean_absolute_error(y_test,tpred_rf)
+
+# combining models to check for possible improvement
+mean_absolute_error(y_test,(tpred_lm+tpred_rf)/2)
+
+# converting Python object into a byte stream to store it in a file/database (binary serialization)
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
+
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+
+model.predict(np.array(list(X_test.iloc[1,:])).reshape(1,-1))[0]
+
+list(X_test.iloc[1,:])
